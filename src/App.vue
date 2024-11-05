b2ae541f9534d553e6d833e56faa20c67db60d59
haruyoshi <1013557536@qq.com>
2024-11-05 17:59:38 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <RouterView></RouterView>
+  </div>
 </template>
 
 <style scoped></style>
